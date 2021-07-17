cbd66e063360c5e3dc4a7f36f0480197bb70d19e
AEJIJEON <jeonaeji@gist.ac.kr>
2021-07-18 00:44:34 +0900

    fix: new-line error 수정

--- src/components/molecules/InputPair/InputPair.js
+++ src/components/molecules/InputPair/InputPair.js
@@ -1,11 +1,13 @@
-import React from "react";
-import LoginInput from "components/atoms/LoginInput/LoginInput";
+import React from 'react';
+import LoginInput from 'components/atoms/LoginInput/LoginInput';
+
 const InputPair = ({ type1, type2, placeholder1, placeholder2 }) => {
-  return(
+  return (
     <div>
       <LoginInput type={type1} placeholder={placeholder1} />
       <LoginInput type={type2} placeholder={placeholder2} />
-    </div>);
+    </div>
+  );
 };
 
 export default InputPair;
